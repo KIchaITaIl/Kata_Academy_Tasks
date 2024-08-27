0b31c0faa54ff6735a2b79127987f5d8fc7a4f30
KIchaITaIl <53560250+KIchaITaIl@users.noreply.github.com>
2024-08-27 23:34:17 +0700

    Add files via upload

--- calculatorV2.go
+++ calculatorV2.go
@@ -9,113 +9,8 @@ import (
 	"strings"
 )
 
-type RomeNumbers int
-
-const (
-	I RomeNumbers = iota + 1
-	II
-	III
-	IV
-	V
-	VI
-	VII
-	VIII
-	IX
-	X
-	XI
-	XII
-	XIII
-	XIV
-	XV
-	XVI
-	XVII
-	XVIII
-	XIX
-	XX
-	XXI
-	XXII
-	XXIII
-	XXIV
-	XXV
-	XXVI
-	XXVII
-	XXVIII
-	XXIX
-	XXX
-	XXXI
-	XXXII
-	XXXIII
-	XXXIV
-	XXXV
-	XXXVI
-	XXXVII
-	XXXVIII
-	XXXIX
-	XL
-	XLI
-	XLII
-	XLIII
-	XLIV
-	XLV
-	XLVI
-	XLVII
-	XLVIII
-	XLIX
-	L
-	LI
-	LII
-	LIII
-	LIV
-	LV
-	LVI
-	LVII
-	LVIII
-	LIX
-	LX
-	LXI
-	LXII
-	LXIII
-	LXIV
-	LXV
-	LXVI
-	LXVII
-	LXVIII
-	LXIX
-	LXX
-	LXXI
-	LXXII
-	LXXIII
-	LXXIV
-	LXXV
-	LXXVI
-	LXXVII
-	LXXVIII
-	LXXIX
-	LXXX
-	LXXXI
-	LXXXII
-	LXXXIII
-	LXXXIV
-	LXXXV
-	LXXXVI
-	LXXXVII
-	LXXXVIII
-	LXXXIX
-	XC
-	XCI
-	XCII
-	XCIII
-	XCIV
-	XCV
-	XCVI
-	XCVII
-	XCVIII
-	XCIX
-	C
-)
-
 func main() {
-	fmt.Println("Привет Kata Academy")
+	fmt.Println("Привет Kata Academy!")
 	fmt.Println("Введите A и B")
 
 	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
@@ -210,6 +105,18 @@ func romanToInt(s string) (int, error) {
 	romanMap := map[rune]int{
 		'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
 	}
+	romanArrCheck := [...]string{
+		"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
+	}
+	romanCheck := false
+	for i := 0; i < len(romanArrCheck); i++ {
+		if s == romanArrCheck[i] {
+			romanCheck = true
+		}
+	}
+	if !romanCheck {
+		return 0, errors.New("Некорректная римская цифра")
+	}
 	total := 0
 	prevValue := 0
 
@@ -225,8 +132,8 @@ func romanToInt(s string) (int, error) {
 		}
 		prevValue = value
 	}
-
 	return total, nil
+
 }
 
 func intToRoman(num int) string {
